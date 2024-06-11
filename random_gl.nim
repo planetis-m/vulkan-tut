42084edeabd22ac719954f93c385ae14d159cb0b
Antonis Geralis <b3lievem@yandex.com>
2024-06-11 17:01:31 +0300

    refactor

--- random_gl.nim
+++ random_gl.nim
@@ -4,15 +4,18 @@ const
   WorkgroupSizeX = 32
   SpirvBinary = staticRead("build/shaders/rand_normal.spv")
 
+type
+  SpecializationConstant = tuple[index, value: GLuint]
+
 proc checkShaderCompilation(shader: GLuint) =
   var status: GLint
   glGetShaderiv(shader, GL_COMPILE_STATUS, addr status)
-  if status == GL_FALSE.Glint:
+  if status == GL_FALSE.GLint:
     var len: GLint
     glGetShaderiv(shader, GL_INFO_LOG_LENGTH, addr len)
     var log = newString(len)
     glGetShaderInfoLog(shader, len, nil, cstring(log))
-    echo "Shader compilation error: ", log
+    raise newException(GLerror, "Shader compilation error: " & log)
 
 proc checkProgramLinking(program: GLuint) =
   var status: GLint
@@ -22,7 +25,38 @@ proc checkProgramLinking(program: GLuint) =
     glGetProgramiv(program, GL_INFO_LOG_LENGTH, addr len)
     var log = newString(len)
     glGetProgramInfoLog(program, len, nil, cstring(log))
-    echo "Program linking error: ", log
+    raise newException(GLerror, "Program linking error: " & log)
+
+proc loadShader[N: static int](shaderType: GLenum, spirvBinary: string,
+                constants: array[N, SpecializationConstant]): GLuint =
+  result = glCreateShader(shaderType)
+  if result != 0.GLUint:
+    glShaderBinary(1, addr result, GL_SHADER_BINARY_FORMAT_SPIR_V,
+                   spirvBinary.cstring, spirvBinary.len.GLsizei)
+    let entryPoint = cstring"main"
+    when N > 0:
+      var indices: array[N, GLuint]
+      var values: array[N, GLuint]
+      for i, constant in constants:
+        indices[i] = constant.index
+        values[i] = constant.value
+      glSpecializeShader(result, entryPoint, constants.len.GLuint, indices[0].addr, values[0].addr)
+    else:
+      glSpecializeShader(result, entryPoint, 0, nil, nil)
+    checkShaderCompilation(result)
+
+proc createComputeProgram[N: static int](spirvBinary: string,
+                          constants: array[N, SpecializationConstant]): GLuint =
+  let module = loadShader(GL_COMPUTE_SHADER, spirvBinary, constants)
+  if module != 0.GLUint:
+    try:
+      result = glCreateProgram()
+      if result != 0.GLUint:
+        glAttachShader(result, module)
+        glLinkProgram(result)
+        checkProgramLinking(result)
+    finally:
+      glDeleteShader(module)
 
 proc main =
   # Create an OpenGL context and window
@@ -40,25 +74,7 @@ proc main =
   echo "OpenGL Version: ", versionString
 
   # Load the compute shader
-  let spirvLength = SpirvBinary.len.GLsizei
-  let constantIndex = 0.GLuint
-  let constantValue = WorkGroupSizeX.GLuint
-  let shaderModule = glCreateShader(GL_COMPUTE_SHADER)
-  glShaderBinary(1, addr shaderModule, GL_SHADER_BINARY_FORMAT_SPIR_V, SpirvBinary.cstring, spirvLength)
-  glSpecializeShader(shaderModule, cstring"main", 1, addr constantIndex, addr constantValue)
-
-  # let shaderCode = readFile("shaders/rand_normal.comp")
-  # glShaderSource(shaderModule, 1, cast[cstringArray](addr shaderCode.cstring), nil)
-  # glCompileShader(shaderModule)
-
-  checkShaderCompilation(shaderModule)
-
-  # Create the shader program
-  var shaderProgram = glCreateProgram()
-  glAttachShader(shaderProgram, shaderModule)
-  glLinkProgram(shaderProgram)
-
-  checkProgramLinking(shaderProgram)
+  let shaderProgram = createComputeProgram(SpirvBinary, {0.GLuint: WorkGroupSizeX.GLuint})
 
   # Use the program
   glUseProgram(shaderProgram)
@@ -101,7 +117,6 @@ proc main =
 
   # Clean up
   glDeleteProgram(shaderProgram)
-  glDeleteShader(shaderModule)
   glDeleteBuffers(1, buffer.addr)
 
 main()
