b0f1ac3f9e9eb839041f71c79609cafb839d3106
Antonis Geralis <b3lievem@yandex.com>
2024-06-11 20:54:20 +0300

    add spirv

--- reduction_gl_naive.nim
+++ reduction_gl_naive.nim
@@ -1,82 +1,15 @@
 # https://medium.com/better-programming/optimizing-parallel-reduction-in-metal-for-apple-m1-8e8677b49b01
-import opengl, opengl/glut, std/[strutils, times]
+import opengl, glut, glerrorcheck, std/[strutils, times]
 
 const
   WorkGroupSize = 256
   NumElements = 1048576
   ElementsPerThread = 1024
   NumWorkGroups = NumElements div (WorkGroupSize * 2 * ElementsPerThread)
+  SpirvBinary = staticRead("build/shaders/reduce.spv")
 
-  ShaderCode = format("""
-#version 460
-
-layout(local_size_x = $1, local_size_y = 1, local_size_z = 1) in;
-
-shared float sharedData[$1];
-
-layout(binding = 0) buffer InputBuffer {
-  float inputData[];
-};
-
-layout(binding = 1) buffer OutputBuffer {
-  float outputData[];
-};
-
-uniform uint n;
-
-void main() {
-  uint localIdx = gl_LocalInvocationID.x;
-  uint localSize = gl_WorkGroupSize.x;
-  uint globalIdx = gl_WorkGroupID.x * localSize * 2 + localIdx;
-  uint gridSize = localSize * 2 * gl_NumWorkGroups.x;
-
-  float sum = 0;
-  while (globalIdx < n) {
-    sum += inputData[globalIdx] + inputData[globalIdx + localSize];
-    inputData[globalIdx] = sum;
-    globalIdx += gridSize;
-  }
-  sharedData[localIdx] = sum;
-  barrier();
-
-  for (uint stride = localSize / 2; stride > 64; stride >>= 1) {
-    if (localIdx < stride) {
-      sum += sharedData[localIdx + stride];
-      sharedData[localIdx] = sum;
-    }
-    memoryBarrierShared();
-  }
-
-  // Final reduction within each subgroup
-  if (localIdx < 64) {
-    sum += sharedData[localIdx + 64];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 32];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 16];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 8];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 4];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 2];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-    sum += sharedData[localIdx + 1];
-    sharedData[localIdx] = sum;
-    memoryBarrierShared();
-  }
-
-  if (localIdx == 0) {
-    outputData[gl_WorkGroupID.x] = sharedData[0];
-  }
-}
-""", WorkGroupSize)
+type
+  SpecializationConstant = tuple[index, value: GLuint]
 
 proc checkShaderCompilation(shader: GLuint) =
   var status: GLint
@@ -86,7 +19,7 @@ proc checkShaderCompilation(shader: GLuint) =
     glGetShaderiv(shader, GL_INFO_LOG_LENGTH, addr len)
     var log = newString(len)
     glGetShaderInfoLog(shader, len, nil, cstring(log))
-    raise newException(GLerror, "Shader compilation error: " & log)
+    raise newException(GLError, "Shader compilation error: " & log)
 
 proc checkProgramLinking(program: GLuint) =
   var status: GLint
@@ -96,22 +29,32 @@ proc checkProgramLinking(program: GLuint) =
     glGetProgramiv(program, GL_INFO_LOG_LENGTH, addr len)
     var log = newString(len)
     glGetProgramInfoLog(program, len, nil, cstring(log))
-    raise newException(GLerror, "Program linking error: " & log)
+    raise newException(GLError, "Program linking error: " & log)
 
-proc loadShader(shaderType: GLenum, source: cstring): GLuint =
-  # Create and compile the compute shader
+proc loadShader[N: static int](shaderType: GLenum, spirvBinary: string,
+                constants: array[N, SpecializationConstant]): GLuint =
   result = glCreateShader(shaderType)
   if result != 0.GLUint:
-    glShaderSource(result, 1, cast[cstringArray](addr source), nil)
-    glCompileShader(result)
+    glShaderBinary(1, addr result, GL_SHADER_BINARY_FORMAT_SPIR_V,
+                   spirvBinary.cstring, spirvBinary.len.GLsizei)
+    let entryPoint = cstring"main"
+    when N > 0:
+      var indices: array[N, GLuint]
+      var values: array[N, GLuint]
+      for i, constant in constants.pairs:
+        indices[i] = constant.index
+        values[i] = constant.value
+      glSpecializeShader(result, entryPoint, constants.len.GLuint, indices[0].addr, values[0].addr)
+    else:
+      glSpecializeShader(result, entryPoint, 0, nil, nil)
     checkShaderCompilation(result)
 
-proc createComputeProgram(computeSource: cstring): GLuint =
-  let module = loadShader(GL_COMPUTE_SHADER, computeSource)
+proc createComputeProgram[N: static int](spirvBinary: string,
+                          constants: array[N, SpecializationConstant]): GLuint =
+  let module = loadShader(GL_COMPUTE_SHADER, spirvBinary, constants)
   if module != 0.GLUint:
     try:
       result = glCreateProgram()
-      # Create the shader program and link the compute shader
       if result != 0.GLUint:
         glAttachShader(result, module)
         glLinkProgram(result)
@@ -123,7 +66,6 @@ proc createGPUBuffer(target: GLenum, size: GLsizeiptr, data: pointer, usage: GLe
   glGenBuffers(1, addr result)
   glBindBuffer(target, result)
   glBufferData(target, size, data, usage)
-  checkGLerror()
 
 type
   Reduction = object
@@ -145,11 +87,11 @@ proc initOpenGLContext() =
   glutInitWindowPosition(50, 50)
   discard glutCreateWindow("OpenGL Compute")
   glutHideWindow()
-  loadExtensions()
+  doAssert glInit(), "Failed to load OpenGL"
 
 proc initResources(): Reduction =
   # Create and compile the compute shader
-  result.program = createComputeProgram(ShaderCode.cstring)
+  result.program = createComputeProgram(SpirvBinary, {0.GLuint: WorkGroupSize.GLuint, 1.GLuint: NumElements})
   # Input buffer
   result.inputBuffer = createGPUBuffer(GL_SHADER_STORAGE_BUFFER, NumElements*sizeof(float32), nil, GL_STATIC_DRAW)
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, result.inputBuffer)
@@ -166,9 +108,6 @@ proc dispatchComputeShader(resources: Reduction) =
   # Bind the shader storage buffers
   glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, resources.inputBuffer)
   glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, resources.outputBuffer)
-  # Get the location of the uniform variable and set it
-  let nLoc = glGetUniformLocation(resources.program, "n")
-  glUniform1ui(nLoc, NumElements)
   # Dispatch the compute shader
   glDispatchCompute(NumWorkGroups, 1, 1)
   # Ensure all work is done
