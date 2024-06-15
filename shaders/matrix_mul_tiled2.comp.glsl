ddc8c264ce22c9ae3cf63531c4d4b91bc33a780a
Antonis Geralis <b3lievem@yandex.com>
2024-06-15 23:53:55 +0300

    update shader

--- shaders/matrix_mul_tiled2.comp.glsl
+++ shaders/matrix_mul_tiled2.comp.glsl
@@ -1,6 +1,6 @@
 #version 460
 
-layout(local_size_x_id = 0, local_size_y_id = 1) in;
+layout(local_size_x_id = 0) in;
 
 layout(binding = 0) buffer ABuffer {
   float A[];
@@ -20,55 +20,50 @@ layout(binding = 3) uniform Parameters {
   int N;
 };
 
-layout(constant_id = 0) const uint TILE_WIDTH_M = 64;
-layout(constant_id = 1) const uint TILE_WIDTH_N = 16;
-const uint TILE_RATIO_K = (TILE_WIDTH_M / TILE_WIDTH_N);
+layout(constant_id = 0) const uint TILE_SIZE_A = 64;
+layout(constant_id = 1) const uint TILE_SIZE_B = 16;
+const uint TILE_SIZE_RATIO = (TILE_SIZE_A / TILE_SIZE_B);
 
-shared float sharedB[TILE_RATIO_K * TILE_WIDTH_N];
+shared float sharedB[TILE_SIZE_RATIO * TILE_SIZE_B];
 
 void main() {
-  uint localRow = gl_LocalInvocationID.x;
-  uint localCol = gl_LocalInvocationID.y;
-  uint globalRow = gl_WorkGroupID.x * gl_WorkGroupSize.x + localRow;
-  uint globalCol = gl_WorkGroupID.y * gl_WorkGroupSize.y + localCol;
+  uint globalRow = gl_WorkGroupID.x * gl_WorkGroupSize.x + gl_LocalInvocationID.x;
+  uint globalCol = gl_WorkGroupID.y * TILE_SIZE_B;
 
-  float cReg[TILE_WIDTH_N];
-  for (uint i = 0; i < TILE_WIDTH_N; ++i) {
+  float cReg[TILE_SIZE_B];
+  for (uint i = 0; i < TILE_SIZE_B; i++) {
     cReg[i] = 0.0;
   }
 
-  for (uint tileIndex = 0; tileIndex < (K + TILE_RATIO_K - 1) / TILE_RATIO_K; tileIndex++) {
+  for (uint tileIndex = 0; tileIndex < (K + TILE_SIZE_RATIO - 1) / TILE_SIZE_RATIO; tileIndex++) {
     // Load tile into shared memory
-    if ((tileIndex * TILE_RATIO_K + localRow) < K) {
-      sharedB[localCol * TILE_RATIO_K + localRow] = B[(tileIndex * TILE_RATIO_K + localRow) * N + globalCol];
+    const uint i = gl_LocalInvocationID.x / TILE_SIZE_B;
+    const uint j = gl_LocalInvocationID.x % TILE_SIZE_B;
+    if (globalCol + j < N && (tileIndex * TILE_SIZE_RATIO + i) < K) {
+      sharedB[i * TILE_SIZE_B + j] = B[(tileIndex * TILE_SIZE_RATIO + i) * N + globalCol + j];
     } else {
-      sharedB[localCol * TILE_RATIO_K + localRow] = 0.0;
+      sharedB[i * TILE_SIZE_B + j] = 0.0;
     }
-
     // Wait for the tile to be loaded before doing computation
     barrier();
 
-    for (uint i = 0; i < TILE_RATIO_K; ++i) {
-      // Load tile of matrix M into register
-      float aVal = 0.0;
-      if (globalRow < M && (tileIndex * TILE_RATIO_K + i) < K) {
-        aVal = A[globalRow * K + (tileIndex * TILE_RATIO_K + i)];
+    for (uint i = 0; i < TILE_SIZE_RATIO; i++) {
+      // Load tile of matrix A into register
+      float aReg = 0.0;
+      if (globalRow < M && (tileIndex * TILE_SIZE_RATIO + i) < K) {
+        aReg = A[globalRow * K + (tileIndex * TILE_SIZE_RATIO + i)];
       }
-
       // Loop over and update the output elements
-      for (uint j = 0; j < TILE_WIDTH_N; ++j) {
-        if (globalCol + j < N) {
-          cReg[j] += aVal * sharedB[i * TILE_RATIO_K + j];
-        }
+      for (uint j = 0; j < TILE_SIZE_B; j++) {
+        cReg[j] += aReg * sharedB[i * TILE_SIZE_B + j];
       }
     }
-
     // Wait for all threads to finish using current tiles before loading new tiles
     barrier();
   }
 
   // Store the output array variable to P elements
-  for (uint j = 0; j < TILE_WIDTH_N; ++j) {
+  for (uint j = 0; j < TILE_SIZE_B; j++) {
     if (globalRow < M && globalCol + j < N) {
       C[globalRow * N + globalCol + j] = cReg[j];
     }
