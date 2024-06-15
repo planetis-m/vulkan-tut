1f54d08087dbbc2deafe4d7449089efc06f5b747
Antonis Geralis <b3lievem@yandex.com>
2024-06-15 22:51:04 +0300

    finally names that make sense

--- mul_cpu2.nim
+++ mul_cpu2.nim
@@ -1,5 +1,6 @@
 # Compile with at least `-d:ThreadPoolSize=workgroupSizeX*workgroupSizeY+1`
 # https://youtu.be/watch?v=jWmtNGqub8c
+# https://github.com/cwpearson/nvidia-performance-tools
 import std/[math, strutils], threading/barrier, malebolgia, malebolgia/lockers
 
 type
@@ -28,45 +29,45 @@ proc wait*(m: BarrierHandle) {.inline.} =
 proc multiplyShader(env: GlEnvironment; barrier: BarrierHandle;
                     buffers: Locker[tuple[A, B, C: seq[float32]]];
                     sharedB: ptr seq[float32]; M, K, N,
-                    tileWidthN, tileWidthM, tileWidthRatioK: int) {.gcsafe.} =
+                    tileSizeB, tileSizeA, tileSizeRatio: int) {.gcsafe.} =
   let localRow = env.gl_LocalInvocationID.y.int
   let localCol = env.gl_LocalInvocationID.x.int
   let globalRow = env.gl_WorkGroupID.y.int * env.gl_WorkGroupSize.y.int + localRow
   let globalCol = env.gl_WorkGroupID.x.int * env.gl_WorkGroupSize.x.int + localCol
 
-  var cReg = newSeq[float32](tileWidthN)
-  # for i in 0..<tileWidthN:
+  var cReg = newSeq[float32](tileSizeB)
+  # for i in 0..<tileSizeB:
   #   cReg[i] = 0
-  for tileIndex in countup(0, ceilDiv(K, tileWidthRatioK)):
+  for tileIndex in countup(0, ceilDiv(K, tileSizeRatio)):
     # Load tiles into shared memory
     unprotected buffers as b:
-      if globalCol < N and (tileIndex * tileWidthRatioK + localRow) < K:
-        sharedB[localRow * tileWidthN + localCol] = b.B[(tileIndex * tileWidthRatioK + localRow) * N + globalCol]
+      if globalCol < N and (tileIndex * tileSizeRatio + localRow) < K:
+        sharedB[localRow * tileSizeB + localCol] = b.B[(tileIndex * tileSizeRatio + localRow) * N + globalCol]
       else:
-        sharedB[localRow * tileWidthN + localCol] = 0
+        sharedB[localRow * tileSizeB + localCol] = 0
     # Wait for both tiles to be loaded in before doing computation
     wait barrier
-    for i in 0..<tileWidthRatioK:
+    for i in 0..<tileSizeRatio:
       # Load tile of matrix M into register
       var aVal: float32 = 0
       unprotected buffers as b:
-        if globalRow < M and (tileIndex * tileWidthRatioK + i) < K:
-          aVal = b.A[globalRow * K + tileIndex * tileWidthRatioK + i]
+        if globalRow < M and (tileIndex * tileSizeRatio + i) < K:
+          aVal = b.A[globalRow * K + tileIndex * tileSizeRatio + i]
       # Loop over and update the output elements
-      for j in 0..<tileWidthN:
+      for j in 0..<tileSizeB:
         if globalCol + j < N:
-          cReg[j] += aVal * sharedB[i * tileWidthN + j]
+          cReg[j] += aVal * sharedB[i * tileSizeB + j]
     # Wait for all threads to finish using current tiles before loading in new
     wait barrier
 
   unprotected buffers as b:
-    for j in 0..<tileWidthN:
+    for j in 0..<tileSizeB:
       if globalRow < M and globalCol + j < N:
         b.C[globalRow * N + globalCol + j] = cReg[j]
 
 proc runComputeOnCpu(numWorkGroups, workGroupSize: UVec3;
                      buffers: Locker[tuple[A, B, C: seq[float32]]]; M, K, N,
-                     tileWidthN, tileWidthM, tileWidthRatioK: int) =
+                     tileSizeB, tileSizeA, tileSizeRatio: int) =
   var env: GlEnvironment
   env.gl_NumWorkGroups = numWorkGroups
   env.gl_WorkGroupSize = workGroupSize
@@ -76,7 +77,7 @@ proc runComputeOnCpu(numWorkGroups, workGroupSize: UVec3;
       for wgX in 0 ..< numWorkGroups.x:
         env.gl_WorkGroupID = uvec3(wgX, wgY, wgZ)
         # echo "New workgroup! id ", wgX, ", ", wgY
-        var shared = newSeq[float32](tileWidthRatioK * tileWidthN)
+        var shared = newSeq[float32](tileSizeRatio * tileSizeB)
 
         var barrier = createBarrier(workGroupSize.x * workGroupSize.y)
         var master = createMaster(activeProducer = true)
@@ -92,7 +93,7 @@ proc runComputeOnCpu(numWorkGroups, workGroupSize: UVec3;
                 )
                 master.spawn multiplyShader(env, barrier.getHandle(), buffers,
                                             addr shared, M, K, N,
-                                            tileWidthN, tileWidthM, tileWidthRatioK)
+                                            tileSizeB, tileSizeA, tileSizeRatio)
 
 # Main
 const
