cc924f2ff60db6370278b979827549dce62d4094
Antonis Geralis <b3lievem@yandex.com>
2024-07-26 16:50:57 +0300

    add vulkan shaderc module

---
+++ vulkan_shaderc.nim
@@ -0,0 +1,24 @@
+import vulkan, vulkan_wrapper, shaderc
+
+proc createShaderModule*(device: VkDevice, source: string,
+    kind: ShadercShaderKind, filename: string, optimize: bool): VkShaderModule =
+  let compiler = shadercCompilerInitialize()
+  let options = shadercCompileOptionsInitialize()
+  try:
+    setTargetEnv(options, Opengl, 0)
+    if optimize:
+      setOptimizationLevel(options, Size)
+    let module = compileIntoSpv(compiler, source.cstring, source.len.csize_t,
+      kind, filename, "main", options)
+    try:
+      if module.getCompilationStatus() != Success:
+        raise newException(ValueError, "Error compiling module - " & $module.getErrorMessage())
+      let shaderModuleCreateInfo = newVkShaderModuleCreateInfo(
+        code = toOpenArray(module.getBytes(), 0, module.getLength().int - 1)
+      )
+      result = createShaderModule(device, shaderModuleCreateInfo)
+    finally:
+      module.release()
+  finally:
+    options.release()
+    compiler.release()
