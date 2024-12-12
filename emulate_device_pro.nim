5c8087be62f614fdfce99661ca71cd2157586338
Antonis Geralis <b3lievem@yandex.com>
2024-12-12 16:02:27 +0200

    replace with better version

--- emulate_device_pro.nim
+++ emulate_device_pro.nim
@@ -3,7 +3,7 @@
 
 ## ## Description
 ##
-## `runComputeOnCpu` is a template that simulates a GPU-like compute environment on the CPU.
+## `runComputeOnCpu` is a function that simulates a GPU-like compute environment on the CPU.
 ## It organizes work into workgroups and invocations, similar to how compute shaders operate
 ## on GPUs.
 ##
@@ -15,34 +15,55 @@
 ##
 ## - `numWorkGroups: UVec3` The number of workgroups in each dimension (x, y, z).
 ## - `workGroupSize: UVec3` The size of each workgroup in each dimension (x, y, z).
-## - `smem: untyped` Defines the shared memory for each workgroup.
-## - `compute: untyped` A call to a compute shader function.
+## - `compute: ComputeProc[A, B, C]` The compute shader procedure to execute.
+## - `ssbo: A` Storage buffer object(s) containing the data to process.
+## - `smem: B` Shared memory for each workgroup.
+## - `args: C` Additional arguments passed to the compute shader.
 ##
 ## ## Compute Function Signature
 ##
-## The function called in the `compute` parameter should have the following signature:
+## The compute shader procedure should have the following signature:
 ##
 ## ```nim
-## proc computeFunction(env: GlEnvironment, barrier: BarrierHandle,
-##                      buffers: Locker[YourBufferType], # either Locker[T] or ptr T
-##                      shared: ptr YourSharedMemoryType, # ditto
-##                      #[ ...additional parameters ]#) {.gcsafe.}
+## proc computeFunction[A, B, C](
+##   env: GlEnvironment,
+##   barrier: BarrierHandle,
+##   buffers: A,     # Storage buffer (typically ptr T or Locker[T])
+##   shared: ptr B,  # Shared memory
+##   args: C         # Additional arguments
+## ) {.nimcall.}
 ## ```
 ##
 ## ## Example
 ##
 ## ```nim
-## proc myComputeShader(env: GlEnvironment, barrier: BarrierHandle,
-##                      buffers: Locker[tuple[input, output: seq[float32]]],
-##                      shared: ptr seq[float32], factor: float32) {.gcsafe.} =
+## type
+##   Buffers = ptr tuple[input, output: seq[float32]]
+##   Shared = seq[float32]
+##   Args = object
+##     factor: int32
+##
+## proc myComputeShader(
+##     env: GlEnvironment,
+##     barrier: BarrierHandle,
+##     buffers: Buffers,
+##     shared: ptr Shared,
+##     args: Args) {.nimcall.} =
 ##   # Computation logic here
 ##
-## let numWorkGroups = uvec3(4, 4, 1)
+## let numWorkGroups = uvec3(4, 1, 1)
 ## let workGroupSize = uvec3(256, 1, 1)
-## var buffers = initLocker (input: newSeq[float32](4096), output: newSeq[float32](4096))
+## var buffers: tuple[input, output: seq[float32]]
+## var numElements = 1024
+## var coarseFactor = 4
 ##
-## runComputeOnCpu(numWorkGroups, workGroupSize, newSeq[float32](256)):
-##   myComputeShader(env, barrier.getHandle(), buffers, addr shared, 2.0f)
+## runComputeOnCpu(
+##   numWorkGroups, workGroupSize,
+##   myComputeShader,
+##   addr buffers,
+##   newSeq[float32](workGroupSize.x),
+##   Args(factor: coarseFactor)
+## )
 ## ```
 ##
 ## ## CUDA to GLSL Translation Table
@@ -71,6 +92,14 @@ type
   BarrierHandle* = object
     x: ptr Barrier
 
+  ComputeProc*[A, B, C] = proc(
+    env: GlEnvironment,
+    barrier: BarrierHandle,
+    buffers: A,
+    shared: ptr B,
+    args: C
+  ) {.nimcall.}
+
 proc uvec3*(x, y, z: uint): UVec3 =
   result = UVec3(x: x, y: y, z: z)
 
@@ -87,59 +116,63 @@ proc wait*(m: BarrierHandle) {.inline.} =
 const
   MaxConcurrentWorkGroups {.intdefine.} = 2
 
-template runComputeOnCpu*(numWorkGroups, workGroupSize: UVec3; ssbo, smem, compute: untyped) =
-  block:
-    proc workGroupProc[A, B](wgX, wgY, wgZ: uint; ssbo: A, smem2: B, env: GlEnvironment) {.nimcall.} =
-      # Auxiliary proc for work group management
-      var env {.inject.} = env
-      env.gl_WorkGroupID = uvec3(wgX, wgY, wgZ)
-      # echo "New workgroup! id ", wgX, ", ", wgY
-      var storage {.inject.} = ssbo
-      var shared {.inject.} = smem2
+proc wrapCompute[A, B, C](env: GlEnvironment, barrier: BarrierHandle,
+    compute: ComputeProc[A, B, C], buffers: A, shared: ptr B, args: C) {.gcsafe.} =
+  compute(env, barrier, buffers, shared, args)
 
-      var barrier {.inject.} = createBarrier(
-          env.gl_WorkGroupSize.x * env.gl_WorkGroupSize.y * env.gl_WorkGroupSize.z)
-      # Create master for managing threads
-      var master = createMaster(activeProducer = true)
-      master.awaitAll:
-        for z in 0 ..< env.gl_WorkGroupSize.z:
-          for y in 0 ..< env.gl_WorkGroupSize.y:
-            for x in 0 ..< env.gl_WorkGroupSize.x:
-              env.gl_LocalInvocationID = uvec3(x, y, z)
-              env.gl_GlobalInvocationID = uvec3(
-                wgX * env.gl_WorkGroupSize.x + x,
-                wgY * env.gl_WorkGroupSize.y + y,
-                wgZ * env.gl_WorkGroupSize.z + z
-              )
-              master.spawn compute
+proc workGroupProc[A, B, C](
+    wgX, wgY, wgZ: uint,
+    env: GlEnvironment,
+    compute: ComputeProc[A, B, C],
+    ssbo: A, smem: ptr B, args: C) {.nimcall.} =
+  # Auxiliary proc for work group management
+  var env = env # Shadow for modification
+  env.gl_WorkGroupID = uvec3(wgX, wgY, wgZ)
+  var smem = smem[] # Allocated per work group
 
-    let env2 = GlEnvironment(
-      gl_NumWorkGroups: numWorkGroups,
-      gl_WorkGroupSize: workGroupSize
-    )
-    var smem2 = smem
+  var barrier = createBarrier(
+    env.gl_WorkGroupSize.x * env.gl_WorkGroupSize.y * env.gl_WorkGroupSize.z)
+  # Create master for managing threads
+  var master = createMaster(activeProducer = true)
+  master.awaitAll:
+    for z in 0 ..< env.gl_WorkGroupSize.z:
+      for y in 0 ..< env.gl_WorkGroupSize.y:
+        for x in 0 ..< env.gl_WorkGroupSize.x:
+          env.gl_LocalInvocationID = uvec3(x, y, z)
+          env.gl_GlobalInvocationID = uvec3(
+            wgX * env.gl_WorkGroupSize.x + x,
+            wgY * env.gl_WorkGroupSize.y + y,
+            wgZ * env.gl_WorkGroupSize.z + z
+          )
+          master.spawn wrapCompute(env, barrier.getHandle(), compute, ssbo, addr smem, args)
 
-    let totalGroups = env2.gl_NumWorkGroups.x * env2.gl_NumWorkGroups.y * env2.gl_NumWorkGroups.z
-    let numBatches = ceilDiv(totalGroups, MaxConcurrentWorkGroups)
-    var currentGroup = 0
-    # Initialize workgroup coordinates
-    var wgX: uint = 0
-    var wgY: uint = 0
-    var wgZ: uint = 0
-    # Process workgroups in batches to limit concurrent execution
-    for batch in 0 ..< numBatches:
-      let endGroup = min(currentGroup + MaxConcurrentWorkGroups, totalGroups.int)
-      # Create master for managing work groups
-      var master = createMaster(activeProducer = false) # not synchronized
-      master.awaitAll:
-        while currentGroup < endGroup:
-          master.spawn workGroupProc(wgX, wgY, wgZ, ssbo, smem2, env2)
-          # Increment coordinates, wrapping when needed
-          inc wgX
-          if wgX >= env2.gl_NumWorkGroups.x:
-            wgX = 0
-            inc wgY
-            if wgY >= env2.gl_NumWorkGroups.y:
-              wgY = 0
-              inc wgZ
-          inc currentGroup
+proc runComputeOnCpu*[A, B, C](
+    numWorkGroups, workGroupSize: UVec3,
+    compute: ComputeProc[A, B, C],
+    ssbo: A, smem: B, args: C) =
+  let env = GlEnvironment(
+    gl_NumWorkGroups: numWorkGroups,
+    gl_WorkGroupSize: workGroupSize
+  )
+  let totalGroups = env.gl_NumWorkGroups.x * env.gl_NumWorkGroups.y * env.gl_NumWorkGroups.z
+  let numBatches = ceilDiv(totalGroups, MaxConcurrentWorkGroups)
+  var currentGroup = 0
+  # Initialize workgroup coordinates
+  var wgX, wgY, wgZ: uint = 0
+  # Process workgroups in batches to limit concurrent execution
+  for batch in 0 ..< numBatches:
+    let endGroup = min(currentGroup + MaxConcurrentWorkGroups, totalGroups.int)
+    # Create master for managing work groups
+    var master = createMaster(activeProducer = false) # not synchronized
+    master.awaitAll:
+      while currentGroup < endGroup:
+        master.spawn workGroupProc(wgX, wgY, wgZ, env, compute, ssbo, addr smem, args)
+        # Increment coordinates, wrapping when needed
+        inc wgX
+        if wgX >= env.gl_NumWorkGroups.x:
+          wgX = 0
+          inc wgY
+          if wgY >= env.gl_NumWorkGroups.y:
+            wgY = 0
+            inc wgZ
+        inc currentGroup
